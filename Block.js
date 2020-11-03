d37cea58238e4184a5ed8bc0afddcfb8fdf17c2b
Krithip <70416931+Krithip@users.noreply.github.com>
2020-11-02 19:42:55 -0600

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,22 @@
+class Block {
+    constructor(x, y, width, height) {
+    var options = {
+        'restitution': 0.8, 
+        'friction': 0
+    } 
+    this.body = Bodies.rectangle(x, y, width, height, options); 
+    this.width = width
+    this.height = height
+    World.add(world, this.body);
+    }
+display() {
+    var pos = this.body.position
+    var angle = this.body.angle
+    push();
+    translate(pos.x, pos.y)
+    rotate(angle)
+    //fill("pink")
+    rect(0, 0, this.width, this.height)
+    pop()
+}
+}
